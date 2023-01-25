25644ded84284c39bbcb218474229cc4d1b20607
Joel Kong <joelkong2001@gmail.com>
2023-01-25 13:41:55 +0800

    feat: implemented countuniquevalues solution

--- bigO/frequencyCounter.js
+++ bigO/frequencyCounter.js
@@ -52,3 +52,18 @@ function sumZero(arr) {
     }
   }
 }
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[j] !== arr[i]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+[1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
