1826f3691fd4a94117f7ba0a2be9267a6ec952fc
Joel Kong <joelkong2001@gmail.com>
2023-01-25 15:55:23 +0800

    feat: did up more recursion problems

--- bigO/recursion.js
+++ bigO/recursion.js
@@ -27,3 +27,29 @@ function collectOddValues(arr) {
   helper(arr);
   return result;
 }
+
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+  return base * power(base, exponent - 1);
+}
+
+function productOfArray(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+function recursiveRange(num) {
+  if (num === 0) {
+    return 0;
+  }
+  return num + recursiveRange(num - 1);
+}
+
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
