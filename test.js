ba48200cd08c4e666ceef4c77e9c7d2a843a8002
Joel Kong <joelkong2001@gmail.com>
2023-01-29 10:58:46 +0800

    chore: tested myself on search and sort algos

--- test.js
+++ test.js
@@ -1,39 +1,52 @@
-function binary(arr, val) {
-  let start = 0;
-  let end = arr.length - 1;
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; i++) {
+      if (short[j] !== long[i + j]) {
+        break;
+      }
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
 
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
+function binary(arr, val) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
     if (arr[mid] === val) {
       return mid;
     } else if (arr[mid] < val) {
-      start = mid + 1;
+      left = mid + 1;
     } else {
-      end = mid - 1;
+      right = mid - 1;
     }
   }
-  return -1;
+  return false;
 }
 
 function bubble(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
-    noSwap = true;
-    for (let j = 0; j < i; j++) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
-        noSwap = false;
       }
     }
-    if (noSwap) {
-      break;
-    }
+    if (noSwaps) break;
   }
   return arr;
 }
 
-function selectionSort(arr) {
+function selection(arr) {
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
     for (let j = i + 1; j < arr.length; j++) {
@@ -47,15 +60,56 @@ function selectionSort(arr) {
       arr[lowest] = temp;
     }
   }
+  return arr;
 }
 
 function insertion(arr) {
   for (let i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    currentValue = arr[i];
+    for (let j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
       arr[j + 1] = arr[j];
     }
-    arr[j + 1] = currentVal;
+    arr[j + 1] = currentValue;
   }
   return arr;
 }
+
+function merge(arr1, arr2) {
+  let temp = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      temp.push(arr2[j]);
+      j++;
+    } else {
+      temp.push(arr1[i]);
+      i++;
+    }
+  }
+
+  while (i < arr1.length) {
+    temp.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    temp.push(arr2[j]);
+    j++;
+  }
+
+  return temp;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+[4, 2][(5, 6)][(4, 2, 5, 6)];
