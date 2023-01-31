64b6b919cbb86fd44839f80bc2cda0fbbe4f78d9
Joel Kong <joelkong2001@gmail.com>
2023-01-31 14:59:11 +0800

    feat: implemented pop, shift and unshift to SLL

--- SLL/singlylinkedlist.js
+++ SLL/singlylinkedlist.js
@@ -25,17 +25,51 @@ class SinglyLinkedList {
     return this;
   }
 
-//   traverse() {
-//     let current = this.head
-//     while(current) {
-//         console.log(current.val);
-//         current = current.next
-//     }
-  }
+  //   traverse() {
+  //     let current = this.head
+  //     while(current) {
+  //         console.log(current.val);
+  //         current = current.next
+  //     }
+  //   }
 
   pop() {
-
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
   }
 
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    this.length--;
+    return oldHead;
+  }
 
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
 let list = new SinglyLinkedList();
