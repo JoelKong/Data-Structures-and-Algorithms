5647cafdfebad2bf9579f35630044050dfa2d60e
Joel Kong <joelkong2001@gmail.com>
2023-01-31 15:18:13 +0800

    feat: implemented get and set functions to SLL

--- SLL/singlylinkedlist.js
+++ SLL/singlylinkedlist.js
@@ -71,5 +71,27 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== idx) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(idx, val) {
+    let foundNode = this.get(idx);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert;
 }
 let list = new SinglyLinkedList();
