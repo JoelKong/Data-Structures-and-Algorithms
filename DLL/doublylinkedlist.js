dc10661ee8a0575940aa9129705348ed90118a70
Joel Kong <joelkong2001@gmail.com>
2023-02-04 17:12:58 +0800

    feat: implemented remove in DLL

--- DLL/doublylinkedlist.js
+++ DLL/doublylinkedlist.js
@@ -102,8 +102,8 @@ class DoublyLinkedList {
 
   insert(idx, val) {
     if (idx < 0 || idx > this.length) return false;
-    if (idx === 0) return this.unshift(val);
-    if (idx === this.length) return this.push(val);
+    if (idx === 0) return !!this.unshift(val);
+    if (idx === this.length) return !!this.push(val);
     let newNode = new Node(val);
     let prevNode = this.get(idx - 1);
     let nextNode = prevNode.next;
@@ -113,5 +113,18 @@ class DoublyLinkedList {
     nextNode.prev = newNode;
     this.length++;
     return true;
+  }
+
+  remove(idx) {
+    if (idx < 0 || idx >= this.length) return false;
+    if (idx === 0) return !!this.shift();
+    if (idx === this.length - 1) return !!this.pop();
+    let removedNode = this.get(idx);
+    removedNode.prev.next = removedNode.next;
+    removedNode.next.prev = removedNode.prev;
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length--;
+    return removedNode;
   }
 }
