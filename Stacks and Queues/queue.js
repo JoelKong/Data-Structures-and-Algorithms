791829c63860711573c1e62eb3d6f73a2bcf1fe1
Joel Kong <joelkong2001@gmail.com>
2023-02-05 12:56:46 +0800

    feat: learnt about queues

---
+++ Stacks and Queues/queue.js
@@ -0,0 +1,41 @@
+let q = [];
+q.push("first");
+q.shift();
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    let poppedNode = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return poppedNode.val;
+  }
+}
