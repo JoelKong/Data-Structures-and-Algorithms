1c3310304f0dc1799dceb37610ac0d27cd864bc8
Joel Kong <joelkong2001@gmail.com>
2023-02-08 19:23:00 +0800

    feat: implemented set on hashtable

--- hashtable/hashtable.js
+++ hashtable/hashtable.js
@@ -21,3 +21,31 @@ function hash(key, arrayLen) {
   }
   return total;
 }
+
+//separate chaining
+class HashTable {
+  constructor(size = 53) {
+    //size of array
+    this.keyMap = new Array(size); //make new array of size default 53
+  }
+
+  _hash(key) {
+    let total = 0;
+    let PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+    return index;
+  }
+}
