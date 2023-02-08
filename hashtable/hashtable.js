568ccc29f9601d6ca9ed8a99216d7cfac8d3c9dd
Joel Kong <joelkong2001@gmail.com>
2023-02-08 19:07:33 +0800

    feat: implemented a better method for a hash function

--- hashtable/hashtable.js
+++ hashtable/hashtable.js
@@ -10,4 +10,14 @@ function hash(key, arrayLen) {
   return total;
 }
 
-hash("pink");
+//faster, more random because prime numbers and especially if you use a prime number for the arrayLen, still a bad hash function
+function hash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total + WEIRD_PRIME + value) & arrayLen;
+  }
+  return total;
+}
