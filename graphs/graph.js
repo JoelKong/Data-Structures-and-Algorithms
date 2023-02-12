7da888c770b8418343632a54148d8440f6c2e8ab
Joel Kong <joelkong2001@gmail.com>
2023-02-12 18:00:34 +0800

    feat: implemented dfs recursive in directed graph

--- graphs/graph.js
+++ graphs/graph.js
@@ -36,4 +36,39 @@ class Graph {
     //delete the actual key with the empty array
     delete this.adjacencyList[vertex];
   }
+
+  DFSRecursive(startVertex) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    function dfs(vertex) {
+      if (!adjacencyList[vertex]) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].map((neighbour) => {
+        if (!visited[neighbour]) {
+          return dfs(neighbour);
+        }
+      });
+    }
+    dfs(startVertex);
+    return result;
+  }
 }
+
+let g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+console.log(g.DFSRecursive("A"));
