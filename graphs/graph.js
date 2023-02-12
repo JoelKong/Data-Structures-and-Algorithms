59f3d75731c2bcf77617ff1af40019a1e1ab5cd6
Joel Kong <joelkong2001@gmail.com>
2023-02-12 14:56:51 +0800

    fix: addEdge function

--- graphs/graph.js
+++ graphs/graph.js
@@ -14,8 +14,8 @@ class Graph {
 
   addEdge(vertex1, vertex2) {
     //directed graph just remove one connection
-    this.adjacencyList[v1].push(vertex2);
-    this.adjacencyList[v2].push(vertex1);
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 
   removeEdge(vertex1, vertex2) {
