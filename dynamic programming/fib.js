b6000407d93c9135a1c4c256387be967cf74f82d
Joel Kong <joelkong2001@gmail.com>
2023-02-16 19:04:35 +0800

    feat: implemented memoization for fib

--- dynamic programming/fib.js
+++ dynamic programming/fib.js
@@ -1,4 +1,14 @@
+//O(2 power of n)
 function fib(n) {
   if (n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
+}
+
+//Dynamic Programming using memoization (O(n))
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
 }
