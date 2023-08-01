335163ddf309e974f0c94e31d39866c12e1272d9
Joel Kong <joelkong2001@gmail.com>
2023-08-01 15:18:04 +0800

    feat: implement LL append pop and prepend

---
+++ Python/Linked List/ll.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    # Print List
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    # Append (O(1))
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    # Pop (O(n))
+    def pop(self):
+        if self.length == 0:
+            return None
+
+        popped_node = self.head
+        current_node = self.head
+        while popped_node.next is not None:
+            current_node = popped_node
+            popped_node = popped_node.next
+        self.tail = current_node
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return popped_node.value
+
+    # Prepend (O(1))
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(4)
