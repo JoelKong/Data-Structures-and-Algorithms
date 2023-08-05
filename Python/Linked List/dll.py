3baad4ccb0071a0468ab27a6ffe8050638928321
Joel Kong <joelkong2001@gmail.com>
2023-08-05 13:30:07 +0800

    feat: implement append in dll

--- Python/Linked List/dll.py
+++ Python/Linked List/dll.py
@@ -18,5 +18,18 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
+    # Append
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
 
 dll = DoublyLinkedList(7)
