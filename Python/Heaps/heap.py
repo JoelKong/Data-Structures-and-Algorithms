6f3f236396947e10b76ca011836d1eff59697345
Joel Kong <joelkong2001@gmail.com>
2023-08-08 14:25:54 +0800

    feat: implemented remove in heap

--- Python/Heaps/heap.py
+++ Python/Heaps/heap.py
@@ -14,6 +14,28 @@ class MaxHeap:
     def _swap(self, index1, index2):
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if self.heap[left_index] > self.heap[max_index] and (
+                left_index < len(self.heap)
+            ):
+                max_index = left_index
+
+            if self.heap[right_index] > self.heap[max_index] and (
+                right_index < len(self.heap)
+            ):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
     def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
@@ -21,3 +43,14 @@ class MaxHeap:
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
+
+    def remove(self):  # only remove top item
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return max_value
