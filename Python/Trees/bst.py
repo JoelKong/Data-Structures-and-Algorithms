c212cfe35456eaa0d6944d150773cd532aa481e0
Joel Kong <joelkong2001@gmail.com>
2023-08-08 20:40:22 +0800

    feat: delete node in bst

--- Python/Trees/bst.py
+++ Python/Trees/bst.py
@@ -36,6 +36,20 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
+
+    def r_insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
     def contains(self, value):
         if self.root is None:
             return False
@@ -61,3 +75,30 @@ class BinarySearchTree:
 
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
+
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:  # last lead
+                return None
+            elif current_node.left == None:  # have right children
+                current_node = current_node.right
+            elif current_node.right == None:  # have left children
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(
+                    current_node.right, sub_tree_min
+                )
+        return current_node
