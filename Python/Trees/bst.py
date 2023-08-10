ec91de421b73015cb5e81d4b3b8dffc32647a6f5
Joel Kong <joelkong2001@gmail.com>
2023-08-10 13:55:23 +0800

    feat: implemented bfs for bst

--- Python/Trees/bst.py
+++ Python/Trees/bst.py
@@ -102,3 +102,19 @@ class BinarySearchTree:
                     current_node.right, sub_tree_min
                 )
         return current_node
+
+    def bfs(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+        return results
